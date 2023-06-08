f13dde83999e988bf96d6be92098a31d8f119b86
Gabriel J. Ferrer <ferrer@hendrix.edu>
2023-06-08 10:00:17 -0500

    At least slightly working sonar program! Needs more testing.

---
+++ gpio_examples/sonar_test.py
@@ -0,0 +1,38 @@
+# Adapted from: https://tutorials-raspberrypi.com/raspberry-pi-ultrasonic-sensor-hc-sr04/
+
+import gpiod
+import time
+
+# trigger is pin 8 - chip 1, line 91
+# echo is pin 10 - chip 1, line 92
+
+
+if __name__ == '__main__':
+
+    with gpiod.Chip('gpiochip1') as chip:
+        trg_line = chip.get_line(91)
+        trg_line.request(consumer='hi', type=gpiod.LINE_REQ_DIR_OUT)
+        ech_line = chip.get_line(92)
+        ech_line.request(consumer='bye', type=gpiod.LINE_REQ_DIR_IN)
+
+        for i in range(4):
+            input("Enter when ready")
+            trg_line.set_value(1)
+            time.sleep(0.00001)
+            trg_line.set_value(0)
+            
+            start = time.time()
+            stop = time.time()
+
+            while ech_line.get_value() == 0:
+                start = time.time()
+
+            while ech_line.get_value() == 1:
+                stop = time.time()
+
+
+            duration = stop - start
+            distance = (duration * 34300) / 2
+
+            print(distance)
+
