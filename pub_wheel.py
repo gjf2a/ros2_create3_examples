b6910ce1644407694a168483541d191fff159f85
Gabriel J. Ferrer <ferrer@hendrix.edu>
2023-06-09 14:53:33 -0500

    Changed it to move forward instead of rotating. Just an experiment.

--- pub_wheel.py
+++ pub_wheel.py
@@ -21,7 +21,8 @@ class WheelPublisher(runner.HdxNode):
         timer_period = 0.5 # seconds
         self.timer = self.create_timer(timer_period, self.timer_callback)
         self.twist = Twist()
-        self.twist.angular.z = 0.628
+        #self.twist.angular.z = 0.628
+        self.twist.linear.x = 1.0
 
     def timer_callback(self):
         self.record_first_callback()
