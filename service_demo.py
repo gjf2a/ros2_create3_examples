66f0f0b8c115b7547420de0d9599c2a860482f47
gjf2a <ferrer@hendrix.edu>
2023-08-04 11:19:02 -0500

    Created service_demo.py.

---
+++ service_demo.py
@@ -0,0 +1,51 @@
+# Goal: Get this command going: ros2 service call /archangel/reset_pose irobot_create_msgs/srv/ResetPose "{pose:{position:{x: 0.0, y: 0.0, z: 0.0}, orientation:{x: 0.0, y: 0.0, z: 0.0, w: 1.0}}}"
+
+import rclpy
+from rclpy.node import Node
+from example_interfaces.srv import AddTwoInts
+import threading, sys
+
+class MinimalService(Node):
+    def __init__(self):
+        super().__init__('minimal_service')
+        self.srv = self.create_service(AddTwoInts, 'add_two_ints', self.add_two_ints_callback)
+
+    def add_two_ints_callback(self, request, response):
+        response.sum = request.a + request.b
+        self.get_logger().info('Incoming request\na: %d b: %d' % (request.a, request.b))
+        return response
+
+
+class MinimalClientAsync(Node):
+    def __init__(self):
+        super().__init__('minimal_client_async')
+        self.cli = self.create_client(AddTwoInts, 'add_two_ints')
+        while not self.cli.wait_for_service(timeout_sec=1.0):
+            self.get_logger().info('service not available, waiting again...')
+        self.req = AddTwoInts.Request()
+
+    def send_request(self, a, b):
+        self.req.a = a
+        self.req.b = b
+        self.future = self.cli.call_async(self.req)
+        rclpy.spin_until_future_complete(self, self.future)
+        return self.future.result()
+
+def main(args=None):
+    rclpy.init(args=args)
+    minimal_service = MinimalService()
+    minimal_client = MinimalClientAsync()
+    executor = rclpy.executors.MultiThreadedExecutor()
+    executor.add_node(minimal_service)
+    executor_thread = threading.Thread(target=executor.spin, daemon=True)
+    executor_thread.start()
+
+    response = minimal_client.send_request(int(sys.argv[1]), int(sys.argv[2]))
+    minimal_client.get_logger().info(
+        'Result of add_two_ints: for %d + %d = %d' %
+        (int(sys.argv[1]), int(sys.argv[2]), response.sum))
+
+    rclpy.shutdown()
+
+if __name__ == '__main__':
+    main()
