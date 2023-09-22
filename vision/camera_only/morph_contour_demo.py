Flood fill centroid calculation is now correct.

vision/camera_only/morph_contour_demo.py
@@ -31,7 +31,6 @@ def morph_contour_loop(video_port, kernel_side, min_space_width, flood, queue):
         if flood and len(close_contour) > 0:
             height, width, _ = frame.shape
             centroid = int(flood_fill(frame, close_contour))
-            print(centroid)
             cv2.line(frame, (centroid, 0), (centroid, height), (0, 0, 255), 1)
             
 
@@ -95,12 +94,18 @@ def find_close_contour(contours, height):
 def flood_fill(frame, close_contour):
     color = (255, 0, 0, 10)
     height, width, _ = frame.shape
-    total = 0
-    for p in close_contour:
+
+    sorted_contour = close_contour[np.argsort(close_contour[:, 0, 0], axis=0)]
+    area = np.sum(height - sorted_contour[:, 0, 1])
+
+    accumulation = 0
+    midpoint = None
+    for p in sorted_contour:
         cv2.line(frame, (p[0][0], p[0][1]), (p[0][0], height), color, 1)
-        total += p[0][0] * p[0][1] / height
-    print(total)
-    return total / len(close_contour)
+        accumulation += height - p[0][1]
+        if midpoint is None and accumulation * 2 > area:
+            midpoint = p[0][0]
+    return midpoint
 
 
 def farthest_x_y(contour):