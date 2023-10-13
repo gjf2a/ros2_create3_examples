00ba25fd093ae7b54ace0c83b834fbdcd65ce212
gjf2a <ferrer@hendrix.edu>
2023-10-13 17:06:31 -0500

    Cool tweak to the ORB matcher - tries to form a groundline from match points

--- vision/remote_orb_matches.py
+++ vision/remote_orb_matches.py
@@ -16,23 +16,28 @@ import numpy as np
 import remote_vision
 
 class OrbDemo(runner.OpenCvCode):
-    def __init__(self, msg_queue):
+    def __init__(self, msg_queue, max_match_distance=10):
         super().__init__(0, self.find_orb, (), msg_queue)
         self.orb = cv2.ORB_create(nfeatures=2000, nlevels=16, edgeThreshold=5)
         self.matcher = cv2.BFMatcher(cv2.NORM_HAMMING, crossCheck=True)
         self.prev_kp = None
         self.prev_des = None
+        self.max_match_distance = max_match_distance
 
     def find_orb(self, frame, cap, other):
         frame = cv2.resize(frame, (320, 240))
         kp, des = self.orb.detectAndCompute(frame, None)
         print(len(kp))
         if self.prev_kp is not None:
-            matches = [m for m in self.matcher.match(des, self.prev_des) if m.distance < 10]
-            for m in matches:
-                x1, y1 = kp[m.queryIdx].pt
-                x2, y2 = self.prev_kp[m.trainIdx].pt
+            matches = sorted([(kp[m.queryIdx].pt, self.prev_kp[m.trainIdx].pt) for m in self.matcher.match(des, self.prev_des) if m.distance < self.max_match_distance], key=lambda p: p[0][0])
+            for ((x1, y1), (x2, y2)) in matches:
                 cv2.line(frame, (int(x1), int(y1)), (int(x2), int(y2)), (0, 0, 255), 1)
+            for i in range(1, len(matches)):
+                x1 = int(matches[i - 1][0][0])
+                y1 = int(matches[i - 1][0][1])
+                x2 = int(matches[i][0][0])
+                y2 = int(matches[i][0][1])
+                cv2.line(frame, (x1, y1), (x2, y2), (255, 0, 0), 1)
             
         self.prev_kp = kp
         self.prev_des = des
