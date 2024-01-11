31e35e98c031829c95f89ea61567ac042a5d947f
gjf2a <ferrer@hendrix.edu>
2024-01-11 16:49:30 -0600

    Fun with menus in curses.

--- curses_menu_demo.py
+++ curses_menu_demo.py
@@ -1,29 +1,46 @@
 from curses import wrapper, curs_set, A_REVERSE
 
-items = ['bell', 'book', 'candle', 'exit']
+
+class MenuItems:
+    def __init__(self, start_row, items):
+        self.items = items
+        self.start_row = start_row
+        self.current = 0
+        self.longest = max(len(item) for item in items)
+
+    def show(self, stdscr):
+        for i, item in enumerate(self.items):
+            item = f"{item}{' ' * (self.longest - len(item))}"
+            if self.current == i:
+                stdscr.addstr(i, 0, item, A_REVERSE)
+            else:
+                stdscr.addstr(i, 0, item)
+
+    def update_from_key(self, key):
+        if key == '\n':
+            return self.items[self.current]
+        elif key == 'KEY_DOWN':
+            self.current = (self.current + 1) % len(self.items)
+        elif key == 'KEY_UP':
+            self.current = (self.current - 1) % len(self.items)
+
+
 
 def main(stdscr):
     curs_set(0)
     stdscr.clear()
 
-    current = 0
+    menu = MenuItems(0, ['bell', 'book', 'candle', 'exit'])
 
     while True:
         stdscr.clear()
-        for i, item in enumerate(items):
-            if current == i:
-                stdscr.addstr(i, 0, item, A_REVERSE)
-            else:
-                stdscr.addstr(i, 0, item)
+        menu.show(stdscr)
         stdscr.refresh()
 
         key = stdscr.getkey()
-        if key == '\n' and current == len(items) - 1:
+        selection = menu.update_from_key(key)
+        if selection == 'exit':
             break
-        elif key == 'KEY_DOWN':
-            current = (current + 1) % len(items)
-        elif key == 'KEY_UP':
-            current = (current - 1) % len(items)
     
 
 if __name__ == '__main__':
