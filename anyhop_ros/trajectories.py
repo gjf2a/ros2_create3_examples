0abe0b96c7cce53d1e980b1ccf90ecbb995b7966
gjf2a <ferrer@hendrix.edu>
2024-07-26 16:26:10 -0500

    Built trajectory mapper.

---
+++ anyhop_ros/trajectories.py
@@ -0,0 +1,46 @@
+import math
+from typing import Tuple, List
+
+import runner
+
+HEADING_TOLERANCE = math.pi / 32
+
+
+class TrajectoryMap:
+    def __init__(self, start=None, collisions=None, prev=None, current=None):
+        self.start = start
+        self.collisions = [] if collisions is None else collisions
+        self.prev = prev
+        self.current = current
+
+    def __repr__(self):
+        return f"TrajectoryMap({self.start}, {self.collisions}, {self.prev}, {self.current})"
+
+    def is_started(self) -> bool:
+        return self.start is not None
+
+    def has_trajectory(self) -> bool:
+        return self.prev is not None
+
+    def update(self, x: float, y: float):
+        if self.is_started():
+            if self.has_trajectory():
+                px, py = self.prev
+                cx, cy = self.current
+                prev_trajectory = math.atan2(cy - py, cx - px)
+                current_trajectory = math.atan2(y - cy, x - cx)
+                if abs(runner.angle_diff(prev_trajectory, current_trajectory)) > HEADING_TOLERANCE:
+                    self.collisions.append(self.current)
+            self.prev = self.current
+            self.current = (x, y)
+        else:
+            self.start = self.current = (x, y)
+
+    def all_points(self) -> List[Tuple[float, float]]:
+        if self.is_started():
+            result = [self.start] + self.collisions
+            if self.has_trajectory():
+                result.extend([self.prev, self.current])
+            return result
+        else:
+            return []
---
+++ anyhop_ros/trajectory_mapper.py
@@ -0,0 +1,47 @@
+import sys
+import runner
+import trajectories
+import rclpy
+from geometry_msgs.msg import Twist
+from nav_msgs.msg import Odometry
+
+from ir_turn import IrTurnNode
+from bump_turn import BumpTurnNode
+
+
+class TrajectoryMapper(runner.WheelMonitorNode):
+    def __init__(self, namespace, ir_limit):
+        super().__init__('trajectory_mapper', namespace)
+        self.publisher = self.create_publisher(Twist, namespace + '/cmd_vel', 10)
+        self.ir_node = IrTurnNode(namespace, ir_limit)
+        self.bump_node = BumpTurnNode(namespace)
+        self.subscribe_odom(self.odom_callback)
+        self.map = trajectories.TrajectoryMap()
+
+    def odom_callback(self, msg: Odometry):
+        x, y = msg.pose.pose
+        self.map.update(x, y)
+        self.record_first_callback()
+        if not self.bump_node.is_turning():
+            if self.bump_node.bump_clear():
+                if not self.ir_node.is_turning():
+                    if self.ir_node.ir_clear():
+                        self.publisher.publish(runner.straight_twist(0.5))
+                    elif self.wheels_stopped():
+                        self.ir_node.start_turn_until_clear()
+            elif self.wheels_stopped():
+                self.bump_node.start_turn()
+
+    def add_self_recursive(self, executor):
+        executor.add_node(self)
+        self.bump_node.add_self_recursive(executor)
+        self.ir_node.add_self_recursive(executor)
+
+
+if __name__ == '__main__':
+    rclpy.init()
+    ir_limit = 50 if len(sys.argv) < 3 else int(sys.argv[2])
+
+    bot = TrajectoryMapper(f'/{sys.argv[1]}', ir_limit)
+    runner.run_recursive_node(bot)
+    print(bot.map.all_points())
