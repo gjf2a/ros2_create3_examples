7ff046d3df556db3a98287a51d5f7349b23f322c
gjf2a <ferrer@hendrix.edu>
2024-07-31 12:30:56 -0500

    Refactored remote_wanderer.py more.

--- remote_wanderer.py
+++ remote_wanderer.py
@@ -53,9 +53,9 @@ def main(stdscr):
 
     while running.is_set():
         get_cmd(stdscr, cmd_queue, running)
-        display_pose(stdscr, pos_queue)
-        display_ir(stdscr, ir_queue)
-        display_bump(stdscr, bump_queue, bump_list)
+        display_pose(stdscr, pos_queue, 2)
+        display_ir(stdscr, ir_queue, 7)
+        display_bump(stdscr, bump_queue, bump_list, 8)
         stdscr.refresh()
 
     st.join()
@@ -72,33 +72,33 @@ def get_cmd(stdscr, cmd_queue, running):
         pass
 
 
-def display_pose(stdscr, pos_queue):
+def display_pose(stdscr, pos_queue, start_line):
     pose = runner.drain_queue(pos_queue)
     if pose:
         if type(pose) == str:
-            stdscr.addstr(5, 0, f"str: {pose}")
+            stdscr.addstr(start_line + 3, 0, f"str: {pose}")
         if type(pose) == Pose:
             p = pose.position
             h = pose.orientation
-            stdscr.addstr(3, 0, f"Position:    ({p.x:6.2f}, {p.y:6.2f}, {p.z:6.2f})        ")
-            stdscr.addstr(4, 0, f"Orientation: ({h.x:6.2f}, {h.y:6.2f}, {h.z:6.2f}, {h.w:6.2f})        ")
+            stdscr.addstr(start_line + 1, 0, f"Position:    ({p.x:6.2f}, {p.y:6.2f}, {p.z:6.2f})        ")
+            stdscr.addstr(start_line + 2, 0, f"Orientation: ({h.x:6.2f}, {h.y:6.2f}, {h.z:6.2f}, {h.w:6.2f})        ")
         elif type(pose) == float:
-            stdscr.addstr(2, 0, f"{pose:.2f}")
+            stdscr.addstr(start_line, 0, f"{pose:.2f}")
         else:
-            stdscr.addstr(7, 0, f"{type(pose)} {pose}")
+            stdscr.addstr(start_line + 4, 0, f"{type(pose)} {pose}")
 
 
-def display_ir(stdscr, ir_queue):
+def display_ir(stdscr, ir_queue, start_line):
     ir = runner.drain_queue(ir_queue)
     if ir:
-        stdscr.addstr(6, 0, f"ir: {ir}{' ' * 30}")
+        stdscr.addstr(start_line, 0, f"ir: {ir}{' ' * 30}")
 
 
-def display_bump(stdscr, bump_queue, bump_list):
+def display_bump(stdscr, bump_queue, bump_list, start_line):
     if not bump_queue.empty():
         b = bump_queue.get()
         bump_list.append(b)
-        stdscr.addstr(8, 0, f"{bump_list}")
+        stdscr.addstr(start_line, 0, f"{bump_list[-3:]}")
 
 
 if __name__ == '__main__':
