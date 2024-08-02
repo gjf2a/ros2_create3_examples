08345c5fae56d25041668ebf26c3958871e22d57
gjf2a <ferrer@hendrix.edu>
2024-08-02 10:32:27 -0500

    Fixed bug.

--- anyhop_ros/print_trajectory_map.py
+++ anyhop_ros/print_trajectory_map.py
@@ -1,5 +1,6 @@
+from trajectories import TrajectoryMap
 import sys
 
-with open(sys.argv[1], 'w') as map_file:
+with open(sys.argv[1], 'r') as map_file:
     tmap = eval(map_file.read())
     print(tmap.all_points())
