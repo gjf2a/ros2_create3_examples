5faf494662a5889b5c8c88f34b81c9d991d61633
gjf2a <ferrer@hendrix.edu>
2024-08-12 16:00:03 -0500

    Trajectory mapper now allows location labels.

--- anyhop_ros/trajectory_mapper.py
+++ anyhop_ros/trajectory_mapper.py
@@ -1,4 +1,4 @@
-import sys, datetime
+import sys, datetime, threading
 import runner
 import trajectories
 import rclpy
@@ -24,6 +24,35 @@ if __name__ == '__main__':
     ir_limit = 50 if len(sys.argv) < 3 else int(sys.argv[2])
 
     bot = TrajectoryMapper(f'/{sys.argv[1]}', ir_limit)
-    runner.run_recursive_node(bot)
+
+    executor = rclpy.executors.MultiThreadedExecutor()
+    bot.add_self_recursive(executor)
+    executor_thread = threading.Thread(target=executor.spin, daemon=True)
+    executor_thread.start()
+    quit = False
+    while not quit:
+        print("Options")
+        print("1: Pause")
+        print("2: Pause and name location")
+        print("3: Resume")
+        print("4: Quit")
+        option = input("Enter choice: ")
+        if option.isdigit():
+            option = int(option)
+            if option <= 2:
+                bot.pause()
+                if option == 2:
+                    name = input("Enter location name: ")
+                    if len(name) > 0:
+                        bot.map.assign_location_name(name)
+                        print(f"{name} assigned to {bot.map.current}")
+                    else:
+                        print("No name assignment performed")
+            elif option == 3:
+                bot.resume()
+            elif option == 4:
+                quit = True
+    rclpy.shutdown()
+    executor_thread.join()
     with open(f"trajectory_{datetime.datetime.now().strftime('%Y-%m-%d_%H-%M-%S')}", 'w') as file:
         file.write(f"{bot.map}\n")
